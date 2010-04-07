223abbdc60e6078d3392f8c049dd52d5c7d13cd8
thierry <thierry@thierry.(none)>
2010-04-07 15:29:08 +0200

    FEAT: Command line

--- main.c
+++ main.c
@@ -1,21 +1,156 @@
 #include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 #include "pla.h"
 #include "load.h"
 #include "render.h"
 
+void usage(void) {
+	fprintf(stderr,
+		"\n"
+		"pla -i <filename> -o <filename> [-f (eps|png|svg|pdf)]\n"
+		"    [-s yyyymmdd] [-e yyyymmdd]\n"
+		"\n"
+	);
+}
+
+time_t convert_yyymmdd(const char *date)
+{
+	struct tm tm;
+
+	memset(&tm, 0, sizeof(struct tm));
+
+	tm.tm_year = conv(date, 4) - 1900;
+	if (tm.tm_year < 0)
+		return -1;
+
+	tm.tm_mon = conv(date + 4, 2) - 1;
+	if (tm.tm_mon < 0)
+		return -1;
+
+	tm.tm_mday = conv(date + 6, 2);
+	if (tm.tm_mday < 0)
+		return -1;
+
+	return mktime(&tm);
+}
+
 int main(int argc, char *argv[])
 {
-	const char *out;
+	const char *out = NULL;
+	const char *in = NULL;
 	char *p;
-	int mode;
+	int mode = 0;
 	struct disp d;
 	struct list_head base = LIST_HEAD_INIT(base);
 	struct task *t;
 	time_t max;
+	int i;
+	time_t start = -1;
+	time_t end = -1;
 
-	pla_load(&base, "out.pla");
+	/* argument parser */
+	for (i=1; i<argc; i++) {
+
+		/* input file */
+		/**/ if (strcmp(argv[i], "-i") == 0) {
+			i++;
+			if (i == argc) {
+				fprintf(stderr, "\nargument -i expect filename\n");
+				usage();
+				exit(1);
+			}
+			in = argv[i];
+		}
+
+		/* output file */
+		else if (strcmp(argv[i], "-o") == 0) {
+			i++;
+			if (i == argc) {
+				fprintf(stderr, "\nargument -o expect filename or -\n");
+				usage();
+				exit(1);
+			}
+			out = argv[i];
+		}
+
+		/* start date for render */
+		else if (strcmp(argv[i], "-s") == 0) {
+			i++;
+			if (i == argc) {
+				fprintf(stderr, "\nargument -s expect start date in format yyymmdd\n");
+				usage();
+				exit(1);
+			}
+			start = convert_yyymmdd(argv[i]);
+			if (start == -1) {
+				fprintf(stderr, "\nargument -s: invalid date\n");
+				usage();
+				exit(1);
+			}
+		}
+
+		/* end date for render */
+		else if (strcmp(argv[i], "-e") == 0) {
+			i++;
+			if (i == argc) {
+				fprintf(stderr, "\nargument -e expect end date in format yyymmdd\n");
+				usage();
+				exit(1);
+			}
+			end = convert_yyymmdd(argv[i]);
+			if (end == -1) {
+				fprintf(stderr, "\nargument -e: invalid date\n");
+				usage();
+				exit(1);
+			}
+			end += 86400;
+		}
+
+		/* format */
+		else if (strcmp(argv[i], "-f") == 0) {
+			i++;
+			if (i == argc) {
+				fprintf(stderr, "\nargument -f expect format (eps, png, pdf or svg)\n");
+				usage();
+				exit(1);
+			}
+			/**/ if (strcasecmp(argv[i], "png") == 0)
+				mode = 1;
+			else if (strcasecmp(argv[i], "eps") == 0)
+				mode = 2;
+			else if (strcasecmp(argv[i], "svg") == 0)
+				mode = 3;
+			else if (strcasecmp(argv[i], "pdf") == 0)
+				mode = 4;
+		}
+
+		/* help */
+		else if (strcmp(argv[i], "-h") == 0) {
+			usage();
+			exit(0);
+		}
+
+	}
+
+	/* checks */
+	if (in == NULL) {
+		fprintf(stderr, "\nsource file expected\n");
+		usage();
+		exit(1);
+	}
+
+	if (out == NULL) {
+		fprintf(stderr, "output file expected\n");
+		usage();
+		exit(1);
+	}
+
+	/* loda planning */
+	pla_load(&base, in);
+
 
 	/* recherche la date la plus petite */
 	d.start = -1;
@@ -49,18 +184,13 @@ int main(int argc, char *argv[])
 	d.duration = max - d.start;
 	d.base = &base;
 
-//	d.start = 1272664800;
-//	d.duration = 2678400;
-//	d.start = 1275602400;
-//	d.duration = 2678400;
-
-	if (argc < 2) {
-		out = "pla.png";
-		mode = 1;
-	}
+	if (start != -1)
+		d.start = start;
 	
-	else {
-		out = argv[1];
+	if (end != -1)
+		d.duration = end - d.start;
+
+	if (mode == 0) {
 		p = strchr(out, '.');
 		if (p == NULL)
 			mode = 1; 
@@ -73,7 +203,8 @@ int main(int argc, char *argv[])
 		else if (strcasecmp(p, ".pdf") == 0)
 			mode = 4;
 		else {
-			fprintf(stderr, "Unknown extension file\n");
+			fprintf(stderr, "Unknown extension file, output format expected. see -f\n");
+			usage();
 			exit(1);
 		}
 	}
